5e226d0efe6cc0e6d51fbf6db3854a1de8d2dd60
a-tiny-flame <atinyflame@gmail.com>
2021-12-20 15:04:56 -0600

    Introducing Maps

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,9 +6,19 @@ import '../components/components.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  static const questions = <String>[
-    'What\'s your favorite color?',
-    'What\'s your favorite animal?'
+  static const questions = <Map<String, Object>>[
+    {
+      'questionTest': 'What\'s your favorite color?',
+      'answwer': <String>['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionTest': 'What\'s your favorite animal?',
+      'answwer': <String>['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionTest': 'What\'s your favorite instructor?',
+      'answwer': <String>['Max', 'Max', 'Max', 'Max'],
+    },
   ];
 
   @override
